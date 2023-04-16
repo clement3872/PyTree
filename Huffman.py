c1aedd5a7cd6bd8ce5dd01c754c1c4c775ab5730
clement3872 <clement.noisy78@gmail.com>
2023-04-16 03:08:42 +0200

    udpate Huffman.py: doing a class

--- Huffman.py
+++ Huffman.py
@@ -1,3 +1,4 @@
+import binTree
 
 
 class Node(object):
@@ -11,68 +12,71 @@ class Node(object):
         self.freq = freq
     
 
-def sort_list_nodes(l):
-    l.sort(key=lambda node:node.freq)
-    return l
-
-def frequence_message(message):
-    # obtenir une liste avec la frequence des caracteres du message
-    d = {el:message.count(el) for el in message}
-    l = [(freq,c) for c,freq in d.items()]
-    return l
-
-
-def merge_nodes(node1, node2):
-    new_node = Node(str(node1.freq+node2.freq), character="", freq=node1.freq+node2.freq,
-                    left=node1, right=node2, parent=None)
-    node1.parent = new_node 
-    node2.parent = new_node
-
-    return new_node
-
-def create_huffman(l_nodes):
-    while len(l_nodes)>1:
-        node1 = l_nodes.pop(0)
-        node2 = l_nodes.pop(0)
-        new_node = merge_nodes(node1, node2)
-        l_nodes = sort_list_nodes(l_nodes)
-        l_nodes.append(new_node)
+class HuffmanTree(binTree.BinaryTree):
+    # This class herits from binTree
+    # note that adding/deleting/changing value of nodes in a Huffman Tree is non-sens
     
-    return l_nodes[0]
-
-
-def width_first_global(node) -> list:
-    if node is None: return 
-    
-    l_nodes = []
-    queue = [node]
-    while queue:
-        node = queue.pop(0)
-        l_nodes.append(node)
+    def __init__(self, message=None):
+        super().__init__()
         
-        if node.left: queue.append(node.left)
-        if node.right: queue.append(node.right) 
+        self.message = message
+        
+        # we use a Node with more attributes different 
+        # than binTree.BinaryTree uses
+        self.root = None; self.init()
+        
     
-    return l_nodes
-
-
-def test(message):
-    freq = frequence_message(message)
-    l_nodes = []
-    for freq,c in freq:
-        n = Node(value=f"{c}:{freq}", character=c, freq=freq)
-        l_nodes.append(n)
+    def open(self, d_tree):
+        # TO DO
+        pass
     
-    l_nodes = sort_list_nodes(l_nodes)
+    def sort_list(self, l):
+        # sort the list on the frequency of each node
+        l.sort(key=lambda node:node.freq)
+        return l
 
-    root = create_huffman(l_nodes)
-    l = width_first_global(root)
-    return root
-    # for node in l:
-    #     print(node.value)
+    def frequence_letters(self):
+        # get a liste with frequencies of each character in the message
+        d = {el:self.message.count(el) for el in self.message}
+        l = [(freq,c) for c,freq in d.items()]
+        return l
 
+    def merge_nodes(self, node1, node2):
+        new_node = Node(str(node1.freq+node2.freq), character="", freq=node1.freq+node2.freq,
+                        left=node1, right=node2, parent=None)
+        node1.parent = new_node 
+        node2.parent = new_node
 
+        return new_node
+
+    def merge_nodes(self, node1, node2):
+        new_node = Node(str(node1.freq+node2.freq), character="", freq=node1.freq+node2.freq,
+                        left=node1, right=node2, parent=None)
+        node1.parent = new_node 
+        node2.parent = new_node
+
+        return new_node
+
+    def init(self):
+        
+        l_nodes = []
+        for freq,c in self.frequence_letters():
+            n = Node(value=f"{c}:{freq}", character=c, freq=freq)
+            l_nodes.append(n)
+        
+        l_nodes = self.sort_list(l_nodes)
+
+        
+        while len(l_nodes)>1:
+            node1 = l_nodes.pop(0)
+            node2 = l_nodes.pop(0)
+            new_node = self.merge_nodes(node1, node2)
+            l_nodes = self.sort_list(l_nodes)
+            l_nodes.append(new_node)
+        
+        self.root = l_nodes[0]
+        
 
 
 if __name__ == '__main__':
-    test("test")
+    t = HuffmanTree("test")
